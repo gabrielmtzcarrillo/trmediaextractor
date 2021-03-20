3e6b50b1cbeeea539fca3fddaf5abaf1dd72e3e7
Gabriel Mtz Carrillo <gabrielmtzcarrillo@users.noreply.github.com>
2021-03-19 20:55:12 -0600

    Project Files
    * Added: CDAudioEx
    * Added; MainSFXEx

---
+++ CDAudioEx/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using GaloreWare.IO;
+
+namespace CDAudioEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "CDAudio.wad Extractor";
+
+            ByteAccess file = new ByteAccess("cdaudio.wad");
+
+            //There should be only 130 entries.
+            for (int i = 0; i < 130; i++)
+            {
+                ExtractTR3Audio(file, i);
+            }
+
+        }//Main
+
+        public static void ExtractTR3Audio(ByteAccess file, int index)
+        {
+            string filename = file.GetASCIIString(index * 0x10C, 260);
+            
+            int size = file.GetInt32((index * 0x10C) + 260);
+            int offset = file.GetInt32((index * 0x10C) + 264);
+
+            if (offset == 0 || size == 0)
+                return;
+
+            if (string.IsNullOrEmpty(filename))
+            {
+                filename = string.Format("untitled{0:00}.wav", index);   
+            }
+
+            Console.WriteLine("EXTRACTING ENTRY {0} \"{1}\"", index, filename);
+            file.SaveOffset(filename, offset, size);
+        }//ExtractTR3Audio
+
+        
+    }
+}
---
+++ MainSFXEx/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using GaloreWare.IO;
+
+namespace MainSFXEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Main.sfx Extractor";
+
+            ByteAccess sfx = new ByteAccess("main.sfx");
+
+            int offset = 0;
+            int index = 1;
+
+            do
+            {
+                offset = ExtractSFX(sfx, offset, string.Format("SFX{0:000}.wav", index));
+                index++;
+            } while (offset > 0);
+
+        }//MAIN
+
+        public static int ExtractSFX(ByteAccess file, int offset, string filename)
+        {
+            if (offset >= file.Length)
+                return -1;
+
+            int size = file.GetInt32(offset + 40);
+
+            Console.WriteLine("EXTRACTING ENTRY {0}",offset);
+
+            file.SaveOffset(filename, offset, 44 + size);
+
+            return offset + 44 + size;
+        }//ExctractSFX
+
+    }
+}
