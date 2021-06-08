d858eed8f3d45bab4d816383c9bb0903b3828545
Gabriel Mtz Carrillo <gabrielmtzcarrillo@users.noreply.github.com>
2021-06-07 22:13:27 -0500

    Moved BMP & TGA classes to GaloreWare.Drawing

--- TGAConv/Program.cs
+++ TGAConv/Program.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.IO;
+using GaloreWare.Drawing.Bitmaps;
+
 namespace TGAConv
 {
     class Program
     {
         static void Main(string[] args)
         {
-            BMP image = new BMP("image.bmp");
-            image.DrawDiagonal();
-            image.Save("TEST.bmp");
+            string[] files = Directory.GetFiles(".", "*.bmp");
+            
+            BmpImage inImage;
+            TgaImage outImage;
 
-            TGA tgaIm = new TGA("level1.tga");
-            tgaIm.DrawDiagonal();
-            tgaIm.Save("TEST.tga");
+            foreach (string file in files)
+            {
+                Console.WriteLine("Converting {0}",file);
+                inImage = new BmpImage(file);
+                outImage = TgaImage.FromBMP(inImage);
+                outImage.Save(file + ".TGA");
+            }
+
+            Console.WriteLine("Done!");
         }
     }
 }
